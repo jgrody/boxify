a6f978aa7419258419d6cba90478aeec0721b192
Jack Grossmann <jtg028@gmail.com>
2015-07-27 14:39:32 -0400

    Prep delete method

--- client/boxes/dashboard/members/controller.ng.js
+++ client/boxes/dashboard/members/controller.ng.js
@@ -1,18 +1,23 @@
 angular.module('boxify').controller('BoxesDashboardMembersController',
-  function($scope, $meteor, box, boxifyDialog){
-    $scope.members = $meteor.collection(function(){
-      return Meteor.users.find({boxId: box._id});
-    }).subscribe('members', box._id)
+function($scope, $meteor, box, boxifyDialog){
+    
+  $scope.members = $meteor.collection(function(){
+    return Meteor.users.find({boxId: box._id});
+  }).subscribe('members', box._id)
 
-    $scope.openAddMemberDialog = function(ev){
-      return boxifyDialog.show({
-        controller: 'DashboardMembersAddController',
-        templateUrl: 'client/boxes/dashboard/members/add/template.ng.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        locals: {
-          box: box
-        }
-      })
-    }
-  });
+  $scope.openAddMemberDialog = function(ev){
+    return boxifyDialog.show({
+      controller: 'DashboardMembersAddController',
+      templateUrl: 'client/boxes/dashboard/members/add/template.ng.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      locals: {
+        box: box
+      }
+    })
+  }
+
+  $scope.deleteUser = function(member){
+    console.log("member: ", member);
+  }
+});
