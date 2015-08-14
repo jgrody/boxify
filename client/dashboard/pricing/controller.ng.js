7d77ec1bdc46d1b65b5554dd09decc780ea5e195
Jack Grossmann <jtg028@gmail.com>
2015-08-13 23:17:58 -0400

    Pricing optimizations

--- client/dashboard/pricing/controller.ng.js
+++ client/dashboard/pricing/controller.ng.js
@@ -1,5 +1,5 @@
 angular.module('boxify').controller('BoxesDashboardPricingController',
-function($scope, $meteor, box, boxifyDialog){
+function($scope, $meteor, box, boxifyDialog, toast){
   window.scope = $scope;
   // Stops meteor reactivity for box so user can continue updating inputs
   // and the browser won't automatically reload on them.
@@ -9,9 +9,9 @@ function($scope, $meteor, box, boxifyDialog){
     checkins: 0,
     price: 0,
     extras: [{}],
-    editing: true
+    editing: true,
+    canDelete: true
   }
-  // var blankTier = angular.copy($scope.tier);
 
   $scope.addNewItem = function(index, last, tier){
     tier.extras[index].canDelete = true;
@@ -23,9 +23,9 @@ function($scope, $meteor, box, boxifyDialog){
 
   $scope.saveTier = function(tier){
     tier = mapTier(tier);
-    tier.extras.each(function(extra){ extra.canDelete = true })
+    tier.extras.each(markAsCanDelete);
     $scope.toggleEditing(tier);
-    box.save();
+    saveBox();
   }
 
   $scope.cancelSave = function(tier){
@@ -55,18 +55,8 @@ function($scope, $meteor, box, boxifyDialog){
     })
   }
 
-  $scope.deleteExtra = function(tier, extra){
-    var confirm = boxifyDialog.confirm()
-      .parent(angular.element(document.body))
-      .title('Are you sure you want to delete this extra?')
-      .ariaLabel('Delete Extra')
-      .ok('Yes')
-      .cancel('No')
-
-    boxifyDialog.show(confirm).then(function(){
-      tier.extras.remove(extra);
-      return box.save();
-    })
+  $scope.deleteExtra = function(tier, $index){
+    tier.extras.removeAt($index);
   }
 
   $scope.addTier = function(){
@@ -79,4 +69,19 @@ function($scope, $meteor, box, boxifyDialog){
     });
     return tier;
   }
+
+  function saveBox(){
+    box.save().then(function(){
+      toast({
+        type: "success",
+        title: "Success",
+        message: "Tier saved."
+      })
+    })
+  }
+
+  function markAsCanDelete(extra){
+    extra.canDelete = true;
+  }
+
 })
